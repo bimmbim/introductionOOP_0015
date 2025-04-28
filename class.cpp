ab8c56995e2ab0698e9603ad66119a4b637f8c25
bimmbim <bimabaraja76@gmail.com>
2025-04-28 11:05:13 +0700

    commit4

--- class.cpp
+++ class.cpp
@@ -7,6 +7,15 @@ class Mobil{
         string tipe_mobil;
     private:
         int harga_mobil;
+    public:
+        void input_data(){
+            cout << "Masukkan nama mobil: ";
+                cin >> nama_mobil;
+            cout << "Masukkan tipe mobil: ";
+                cin >> tipe_mobil;
+            cout << "Masukkan harga mobil: ";
+                cin >> harga_mobil;
+        }
         
 };
 
