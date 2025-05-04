f2da8f48a15806d7b423f20c64fd581ca4ebf7ee
bimmbim <bimabaraja76@gmail.com>
2025-05-04 19:36:11 +0700

    commit11

---
+++ class2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class persegi_panjang{
+    private:
+    int panjang;
+    int lebar;
+    int luas;
+
+    public:
+    void input_data(){
+        cout << "Masukkan panjang: ";
+            cin >> panjang;
+        cout << "Masukkan lebar: ";
+            cin >> lebar;
+            }
+    void output_data(){
+        cout << "Tampilkan luas = " << luas << endl;
+        }
+    void hitung_luas(){
+        luas = panjang * lebar;
+    }
+};
